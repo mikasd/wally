6e11081e53a79bf51de9759a14c08ef24c626854
mingley <54518@icf.com>
2021-09-11 17:37:00 -0700

    implements fltk for desktop gui, adds ability to add wallets, lets you send crypto, basic styling, send/recv channel event response

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,100 @@
 use anyhow::Result;
+use fltk::{
+    app,
+    button::Button,
+    enums::{Color, Font, FrameType},
+    frame::Frame,
+    input,
+    prelude::{GroupExt, InputExt, WidgetBase, WidgetExt},
+    window::Window,
+};
+use secp256k1::{PublicKey, SecretKey};
+use std::str::FromStr;
+use tokio;
+use web3::types::Address;
 
 mod wallet_lib;
+use crate::wallet_lib::{create_keypair, create_txn_object, sign_and_send};
 
-const URL: &str = "a";
-fn main() -> Result<()> {
-    let keypair = wallet_lib::create_keypair();
-    
-    println!("{:?}", keypair);
+const URL: &str = "https://eth-ropsten.alchemyapi.io/v2/KCNPR0famYesyHVy345ynShLVIr9Eaf4";
 
-    let web3 = wallet_lib::setup_web3_connection(url)?;
-    
+#[derive(Debug, Clone)]
+pub enum WalletMessage {
+    NewWallet,
+    Send,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = app::App::default();
+
+    let mut window = Window::default()
+        .with_size(500, 800)
+        .with_label("basic wallet");
+    let mut button_1 = Button::new(195, 450, 120, 45, "Create Wallet");
+    let mut button_2 = Button::new(200, 300, 100, 40, "send");
+    let mut input_1 = input::Input::new(200, 200, 225, 35, "to: ");
+    let mut frame = Frame::default()
+        .with_size(600, 100)
+        .center_of(&window)
+        .with_label("0 wallets");
+
+    frame.set_label_color(Color::White);
+    frame.set_label_font(Font::TimesBold);
+    frame.set_label_size(24);
+
+    window.set_color(Color::DarkCyan);
+
+    button_1.set_color(Color::White);
+    button_1.set_label_color(Color::DarkMagenta);
+    button_1.set_label_font(Font::TimesBold);
+    button_1.set_frame(FrameType::FlatBox);
+    button_1.clear_visible_focus();
+
+    button_2.set_color(Color::White);
+    button_2.set_label_color(Color::DarkMagenta);
+    button_2.set_label_font(Font::TimesBold);
+    button_2.set_frame(FrameType::FlatBox);
+    button_2.clear_visible_focus();
+
+    input_1.set_frame(FrameType::FlatBox);
+
+    window.end();
+    window.show();
+
+    let (s, r) = app::channel::<WalletMessage>();
+
+    button_1.emit(s.clone(), WalletMessage::NewWallet);
+    button_2.emit(s, WalletMessage::Send);
+
+    let web3 = wallet_lib::setup_web3_connection(URL)?;
+    let mut keypairs: Vec<(PublicKey, SecretKey)> = Vec::new();
+
+    while app.wait() {
+        if let Some(msg) = r.recv() {
+            match msg {
+                WalletMessage::NewWallet => {
+                    let (seckey, pubkey) = match create_keypair() {
+                        Ok(val) => val,
+                        Err(_e) => unimplemented!(),
+                    };
+                    keypairs.push((pubkey, seckey));
+                    frame.set_label(&format!("{} Wallets", keypairs.len()));
+                },
+                WalletMessage::Send => {
+                    let to_address = Address::from_str(&input_1.value().as_str())?;
+                    let tx_object = create_txn_object(to_address, 69)?;
+                    let result = sign_and_send(web3.clone(), tx_object, keypairs[0].1).await;
+                    match result {
+                        Ok(val) => frame.set_label(&format!("{}", val)),
+                        Err(e) => frame.set_label(&format!("{}", e)),
+                    };
+                }
+            }
+        }
+    }
+
+    app.run().unwrap();
+
     Ok(())
 }
--- src/wallet_lib.rs
+++ src/wallet_lib.rs
@@ -4,19 +4,28 @@ use secp256k1::{
     rand::{rngs, SeedableRng}
 };
 use anyhow::Result;
-use web3::{
-    self,
-    Web3,
-    transports::Http
-};
+use web3::{self, Web3, transports::Http, types::{H160, H256, TransactionParameters, U256}};
 
 pub fn create_keypair() -> Result<(SecretKey, PublicKey)> {
     let secp = secp256k1::Secp256k1::new();
-    let mut rng = rngs::StdRng::seed_from_u64(3);
+    let mut rng = rngs::StdRng::seed_from_u64(69);
     Ok(secp.generate_keypair(&mut rng))
 }
 
 pub fn setup_web3_connection(url: &str) -> Result<Web3<Http>> {
     let transport = web3::transports::Http::new(url)?;
     Ok(web3::Web3::new(transport))
+}
+
+pub fn create_txn_object( to: H160, value: usize) -> Result<TransactionParameters> {
+    Ok(TransactionParameters{
+        to: Some(to),
+        value: U256::exp10(value),
+        ..Default::default() 
+    })
+}
+
+pub async fn sign_and_send(web3: Web3<Http>, tx_object: TransactionParameters, seckey: SecretKey) -> Result<H256> {
+    let signed = web3.accounts().sign_transaction(tx_object, &seckey).await?;
+    Ok(web3.eth().send_raw_transaction(signed.raw_transaction).await?)
 }
